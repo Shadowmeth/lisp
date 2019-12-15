474b689cf1047d5c645065b628ddf5e28a26b85e
Shadowmeth <47725754+Shadowmeth@users.noreply.github.com>
2019-12-15 19:18:10 +0500

    Add files via upload
    Added lex method, removed AST functionality.

--- main.c
+++ main.c
@@ -1,168 +1,229 @@
-/*
- * =====================================================================================
- *
- *       Filename:  main.c
- *
- *    Description: Lisp compiler 
- *
- *        Version:  1.0
- *        Created:  12/09/2019 09:21:03 AM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  YOUR NAME (Syed Kumail Hussain Sheraz), 
- *   Organization:  
- *
- * =====================================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <stdbool.h>
+#include <ctype.h>
 #include <editline/readline.h>
 
-int lookahead = 0; /* Start at the first char of input */
-char *input;
+typedef struct Token {
 
-struct ast_node {
-	enum Node_Type {
-		NUM,
-		OP,
-	}type;
+	enum Token_Type {
+		PLUS, MINUS, MULTIPLY, DIVIDE, NUMBER, L_PAREN, R_PAREN, INVALID, _EOF
+
+	}T_Type;
 
 	union {
-		int val;
-		int op;
-	}v;
-	
-	struct ast_node *left;
-	struct ast_node *right;
+		long long i;
+		int sym; /* +, -, *, /, (, ), and -1 for EOF */
+	}T_Val;
 
-};
+}Token;
 
-struct ast_node *new_op_node(int op)
+bool match(int ch, char *src) 
 {
-	struct ast_node *new = malloc(sizeof(struct ast_node));
-	new -> type = OP;
-	new -> v.op = op;
-
-	new -> left = NULL;
-	new -> right = NULL;
-
-	return new;
-}
-
-bool number(struct ast_node *n)
-{
-	bool num_found = false;
-	int v = 0;
-
-	if (match('-'))  {	/* negative number, we assume that a number is found after -, if there isn't any, assume 0 */ 
-
-		while (isdigit(input[lookahead])) {
-			v = v * 10 + input[lookahead] - '0';
-			lookahead++;
-		}
-		num_found = true;
-	
-	} else if (isdigit(input[lookahead])) {	/* positive number */
-
-		while (isdigit(input[lookahead])) {
-			v = v * 10 + input[lookahead] - '0';
-			lookahead++;
-		}
-
-		num_found = true;
-	}
-
-	if (num_found) {
-		/* go to the left most leaf of n and add a number */
-			
-
-	}
-}
-
-bool match(int ch)
-{
-	if (input[lookahead] == ch) {
-		/* advance lookahead and return true */
-		lookahead++;
+	if (*src == ch) {
+		src++;
 		return true;
 	}
 
 	return false;
 }
 
-void operator(struct ast_node  *n)
+Token lex(char *src)
 {
-	/* skip extra space */
-	while (isspace(input[lookahead]))
-		lookahead++;
 
-	if (match('+')) {
-		n -> left = NULL;
-		n -> right = new_op_node('+');
-	} else if (match('-')) {
-		n -> left = NULL;
-		n -> right = new_op_node('-');
-	} else if (match('/')) {
-		n -> left = NULL;
-		n -> right = new_op_node('/');
-	} else if (match('*')) {
-		n -> left = NULL;
-		n -> right = new_op_node('*');
-	} else {	/* error */
-		fprintf(stderr, "Error: expected (+, -, /, *) - got %c\n", input[lookahead]);
-		exit(EXIT_FAILURE);
+	Token ret_tok;
+
+	while (*src == ' ' || *src == '\t') {	/* skip whitespace */
+		src++;
 	}
+
+	if (!*src) {
+		ret_tok.T_Type = _EOF;
+		ret_tok.T_Val.sym = -1;
+		src++;
+		return ret_tok;
+
+	} else if (*src == '+') {
+		ret_tok.T_Type = PLUS;
+		ret_tok.T_Val.sym = '+';
+		src++;
+		return ret_tok;
+	
+	} else if (*src == '-') {
+		ret_tok.T_Type = MINUS;
+		ret_tok.T_Val.sym = '*';
+		src++;
+		return ret_tok;
+	
+	} else if (*src == '/') {
+		ret_tok.T_Type = DIVIDE;
+		ret_tok.T_Val.sym = '/';
+		src++;
+		return ret_tok;
+
+	} else if (*src == '*') {
+		ret_tok.T_Type = MULTIPLY;
+		ret_tok.T_Val.sym = '*';
+		src++;
+		return ret_tok;
+
+	} else if (*src == '(') {
+		ret_tok.T_Type = L_PAREN;
+		ret_tok.T_Val.sym = '(';
+		src++;
+		return ret_tok;
+
+	} else if (*src == ')') {
+		ret_tok.T_Type = R_PAREN;
+		ret_tok.T_Val.sym = ')';
+		src++;
+		return ret_tok;
+
+	} else if (isdigit(*src)) { /* we are not considering overflow yet */
+		/* found a number, collect it */
+		int num = 0;
+		while (isdigit(*src)) {
+			num = num * 10 + (*src - '0');
+			src++;
+		}
+
+		ret_tok.T_Type = NUMBER;
+		ret_tok.T_Val.i = num; 
+
+		return ret_tok;
+	}
+
+	/* If we reached here, we found some invalid characters */
+	/* No need to consider the value of the token */
+	ret_tok.T_Type = INVALID;
+	return ret_tok;
 }
 
-bool expr(struct ast_node *n)
+bool number(char *src)
 {
-
-	if (number(n)) { /* if a number is found add to the left of n as a new ast_node */
+	Token t = lex(src);
+	if (t.T_Type == NUMBER) {
 		return true;
-	} else {
-		if (!match('(')); {
-			fprintf(stderr, "Error: expected '(', got %c\n", input[lookahead]);
-			exit(EXIT_FAILURE);
-		}
-		operator(n);
+	} else if (t.T_Type != NUMBER && t.T_Type != INVALID) {
+		fprintf(stderr, "Error: expected number, found %c\n", t.T_Val.sym);
+		return true;
+	} else if (t.T_Type == INVALID) {
+		fprintf(stderr, "Invalid characters found!\n");
+		return false;
 
-		while(expr(n))
-			expr(n);
-	
-		if (!match(')')) {
-			fprintf(stderr, "Error: expected ')', got %c\n", input[lookahead]);
-			exit(EXIT_FAILURE);
-		}
 	}
+
+	return true;
+}
+
+bool operator(char *src)
+{
+	Token t = lex(src);
+
+	if (t.T_Type == _EOF) {
+		fprintf(stderr, "End of line found!\n");
+		return false;
+	}
+
+	if (t.T_Type == INVALID) {
+		fprintf(stderr, "Invalid characters found!\n");
+		return false;
+	}
+
+	if (t.T_Type != PLUS && t.T_Type != MINUS && t.T_Type != MULTIPLY && t.T_Type != DIVIDE) {
+		fprintf(stderr, "Error: Expected an operator (+, -, *, /) - found ");
+		if (t.T_Type == _EOF) {
+			fprintf(stderr, "%d\n", -1); 
+		} else if (t.T_Type == NUMBER) {
+			fprintf(stderr, "%d\n", t.T_Val.i);
+		} else {
+			fprintf(stderr, "%c\n", t.T_Val.sym);
+		}
+		
+		return false;
+	}
+
+	return true;
+
+}
+
+bool expr(char *src)
+{
+	Token t = lex(src);
+
+	if (t.T_Type == _EOF)  {
+		fprintf(stderr, "End of line found!\n");
+		return false;
+	}
+
+	if (t.T_Type == INVALID) {
+		fprintf(stderr, "Invalid characters found!\n");
+		return false;	
+
+	}
+	
+	if (t.T_Type == NUMBER) {
+		return true;
+	}
+
+
+	if (!match('(', src)) {
+		fprintf(stderr, "Error: Expected '(', found %c\n", t.T_Val.sym);
+		return false;
+	}
+
+
+	if (!operator(src)) {
+		return false;
+	}
+
+	do {
+		if (!operator(src)) {
+			return false;
+		}
+
+	} while (expr(src));
+
+	if (!match(')', src)) {
+		fprintf(stderr, "Error: expected ')', found %c\n", t.T_Val.sym);
+		return false;
+	}
+
+	return true;
+}
+
+bool lispy(char *src)
+{
+	if (!operator(src)) {
+		return false;
+	}
+
+	do {
+		if (!expr(src)) {
+			return false;
+		}
+
+	} while(expr(src));
+
+	return true;
 }
 
 int main(int argc, char *argv[])
 {
-	/* Print version and exit info */
 	puts("Lispy version 0.0.1");
 	puts("Press Ctrl+c to exit\n");
 
-	struct ast_node *prog = malloc(sizeof(struct ast_node));
 
 	while (1) {
+		char *input = readline("lispy> ");
 
-		input = readline("lispy> ");
-
-		/* Add input to history */
 		add_history(input);
-	
-		operator(prog);
-
-		while(expr(prog))
-			expr(prog);
-
-		free(input);
-
+		char *free_input = input; /* save address to free the memory, because input is changed */
+		
+		lispy(input);
+		
+		free(free_input);
 	}
 
 	return 0;
